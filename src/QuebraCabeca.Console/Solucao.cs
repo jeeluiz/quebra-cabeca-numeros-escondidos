f73d039d02ca1c411ae643826b29b6f5fdf31756
Jeferson Luiz <jl_jau@hotmail.com>
2025-03-23 08:36:03 -0300

    Fix:Otimizar Loop

--- src/QuebraCabeca.Console/Solucao.cs
+++ src/QuebraCabeca.Console/Solucao.cs
@@ -12,54 +12,60 @@ public static class Solucao
      public static List<int> EncontrarNumerosEscondidosMaiorIgualD(int A, int B, int C, int D, List<int> E)
     {
         List<int> resultado = [];
+
+        int inicio = A;
         
-        for(int i = A; i<=B; i++ )
+        if(inicio % C != 0) 
+        { 
+            inicio += C -inicio % C;
+        }
+
+        for (int i = inicio; i <= B; i+=C)
         {
-            if(i % C != 0)
-            {
-                continue;
-            }
-
-            var iChars = i.ToString().ToCharArray();
-            var somaDigitos =  iChars.Select(x => int.Parse(x.ToString())).Sum();
-
-            if(somaDigitos < D)
-            {
-                continue;
-            }
-            
-            bool contemChar = false;
            
-            foreach(var c in iChars)
+            var iChars = i.ToString().ToCharArray();
+            var somaDigitos = iChars.Select(x => int.Parse(x.ToString())).Sum();
+
+            if (somaDigitos < D)
             {
-                if(E.Contains(int.Parse(c.ToString())))
+                continue;
+            }
+
+            bool contemChar = false;
+
+            foreach (var c in iChars)
+            {
+                if (E.Contains(int.Parse(c.ToString())))
                 {
                     contemChar = true;
                     break;
                 }
             }
-             
-            if(contemChar == true)
+
+            if (contemChar == true)
             {
                 continue;
             }
-            
+
             resultado.Add(i);
         }
         return resultado;
     }
 
      public static List<int> EncontrarNumerosEscondidosMaiorD(int A, int B, int C, int D, List<int> E)
-    {
+     {
         List<int> resultado = [];
-        
-        for(int i = A; i<=B; i++ )
-        {
-            if(i % C != 0)
-            {
-                continue;
-            }
 
+        int inicio = A;
+
+        if (inicio % C != 0)
+        {
+            inicio += C - inicio % C;
+        }
+
+        for (int i = inicio; i <= B; i += C)
+        {
+            
             var iChars = i.ToString().ToCharArray();
             var somaDigitos =  iChars.Select(x => int.Parse(x.ToString())).Sum();
 
